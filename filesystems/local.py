e43414d4195b673cfa186b13879a05d196774598
lockermanwxlf <95518889+lockermanwxlf@users.noreply.github.com>
2024-07-13 07:40:31 -0500

    Add abstraction layer for downloading posts.

---
+++ filesystems/local.py
@@ -0,0 +1,42 @@
+import os
+import requests
+from vsco_api import VscoMedia, VscoMediaType
+import fake_useragent
+
+OUTPUT_FOLDER = 'output'
+
+fake_ua = fake_useragent.FakeUserAgent()
+
+def get_filepath(directory_name: str, post: VscoMedia):
+    global OUTPUT_FOLDER
+    extension = '.png' if post.type == VscoMediaType.IMAGE else '.mp4'
+    filename = f'{directory_name} {post.timestamp}{extension}'
+    os.makedirs(os.path.join(OUTPUT_FOLDER, directory_name), exist_ok=True)
+    return os.path.join(OUTPUT_FOLDER, directory_name, filename)
+
+def process_post(directory_name: str, post: VscoMedia):
+    global fake_ua
+    filepath = get_filepath(directory_name, post)
+    if os.path.exists(filepath): 
+        return
+    match post.type:
+        case VscoMediaType.IMAGE:
+            print('Downloading', directory_name, post.timestamp)
+            response = requests.get(
+                url=post.download_url,
+                headers={
+                    'User-Agent': fake_ua.random
+                })
+            response.raise_for_status()
+            with open(filepath, 'wb+') as file:
+                file.write(response.content)
+        case VscoMediaType.VIDEO_MP4:
+            print('Downloading', directory_name, post.timestamp)
+            response = requests.get(
+                url=post.download_url,
+                headers={
+                    'User-Agent': fake_ua.random
+                })
+            response.raise_for_status()
+            with open(filepath, 'wb+') as file:
+                file.write(response.content)
