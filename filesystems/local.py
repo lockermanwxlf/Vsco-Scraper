a16d156deb57ff5735ea8277f852a4dabe0135b1
lockermanwxlf <95518889+lockermanwxlf@users.noreply.github.com>
2024-07-14 03:37:37 -0500

    Turn local filesystem into class for inheritance of download method. Return boolean indicating whether a file was downloaded.

--- filesystems/local.py
+++ filesystems/local.py
@@ -7,36 +7,38 @@ OUTPUT_FOLDER = 'output'
 
 fake_ua = fake_useragent.FakeUserAgent()
 
-def get_filepath(directory_name: str, post: VscoMedia):
-    global OUTPUT_FOLDER
-    extension = '.png' if post.type == VscoMediaType.IMAGE else '.mp4'
-    filename = f'{directory_name} {post.timestamp}{extension}'
-    os.makedirs(os.path.join(OUTPUT_FOLDER, directory_name), exist_ok=True)
-    return os.path.join(OUTPUT_FOLDER, directory_name, filename)
+class LocalFilesystem:
+    
+    def get_filepath(self, directory_name: str, post: VscoMedia):
+        global OUTPUT_FOLDER
+        extension = '.png' if post.type == VscoMediaType.IMAGE else '.mp4'
+        filename = f'{directory_name} {post.timestamp}{extension}'
+        os.makedirs(os.path.join(OUTPUT_FOLDER, directory_name), exist_ok=True)
+        return os.path.join(OUTPUT_FOLDER, directory_name, filename)
 
-def process_post(directory_name: str, post: VscoMedia):
-    global fake_ua
-    filepath = get_filepath(directory_name, post)
-    if os.path.exists(filepath): 
-        return
-    match post.type:
-        case VscoMediaType.IMAGE:
-            print('Downloading', directory_name, post.timestamp)
-            response = requests.get(
-                url=post.download_url,
-                headers={
-                    'User-Agent': fake_ua.random
-                })
-            response.raise_for_status()
-            with open(filepath, 'wb+') as file:
-                file.write(response.content)
-        case VscoMediaType.VIDEO_MP4:
-            print('Downloading', directory_name, post.timestamp)
-            response = requests.get(
-                url=post.download_url,
-                headers={
-                    'User-Agent': fake_ua.random
-                })
-            response.raise_for_status()
-            with open(filepath, 'wb+') as file:
-                file.write(response.content)
+    def process_post(self, directory_name: str, post: VscoMedia):
+        global fake_ua
+        filepath = self.get_filepath(directory_name, post)
+        if os.path.exists(filepath): 
+            return False
+        print('Downloading', directory_name, post.timestamp)
+        match post.type:
+            case VscoMediaType.IMAGE:
+                response = requests.get(
+                    url=post.download_url,
+                    headers={
+                        'User-Agent': fake_ua.random
+                    })
+                response.raise_for_status()
+                with open(filepath, 'wb+') as file:
+                    file.write(response.content)
+            case VscoMediaType.VIDEO_MP4:
+                response = requests.get(
+                    url=post.download_url,
+                    headers={
+                        'User-Agent': fake_ua.random
+                    })
+                response.raise_for_status()
+                with open(filepath, 'wb+') as file:
+                    file.write(response.content)
+        return True
